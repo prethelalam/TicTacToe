1495bf1b3eb96e9861a9daef9da76fe91ecc306e
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2023-03-02 18:25:39 -0600

    Updated Options

--- TicTacToe.java
+++ TicTacToe.java
@@ -76,20 +76,32 @@ static void Board(){
         Scanner scan1 = new Scanner(System.in);
         int input = scan1.nextInt();
 
-        // System.out.print("Player 2, where would you like your X to be placed: top left, top middle, top riddle, etc.. ");
-        // input = scan1.nextInt();
+        System.out.print("Player 2, where would you like your X to be placed: top left, top middle, top riddle, etc.. ");
+        int input2 = scan1.nextInt();
 
-        switch (counter){ //need to have this put x or o depending on if player 1 or 2 selects this spot
-            case 00: gameBoard[0][0] = 'X' | 'O';
-            for (char [] row: gameBoard){ //so here we are pulling an element from the gameboard and storing each one in row. this for loop is set up to create each row
-            for (char c: row){ //this loops each character in the current row
-                System.out.print(c);
-            }
-            System.out.println();
+        switch (input){ //need to have this put x or o depending on if player 1 or 2 selects this spot
+            case 00: gameBoard[0][0] = 'X';
+                for (char [] row: gameBoard){ //so here we are pulling an element from the gameboard and storing each one in row. this for loop is set up to create each row
+                for (char c: row){ //this loops each character in the current row
+                    System.out.print(c);
+                }
+                System.out.println();
         }
-        }
-        counter++;
     }
+        switch(input2){ //this worked!!!!!
+            case 00: gameBoard[0][0] = 'O';
+                for (char [] row: gameBoard){ //so here we are pulling an element from the gameboard and storing each one in row. this for loop is set up to create each row
+                for (char c: row){ //this loops each character in the current row
+                    System.out.print(c);
+                }
+                System.out.println();
+        }
+            
+        }
+        
+    
+    counter++;
+}
 }
 
 
