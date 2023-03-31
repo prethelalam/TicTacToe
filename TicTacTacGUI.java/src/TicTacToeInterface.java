1d5cfbd00d28a5db283307db0a9315818b168410
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2023-03-30 19:03:29 -0500

    Updated GUI

--- TicTacTacGUI.java/src/TicTacToeInterface.java
+++ TicTacTacGUI.java/src/TicTacToeInterface.java
@@ -5,7 +5,15 @@ import java.awt.event.ActionListener;
 public class TicTacToeInterface {
     private JButton buttonMSG;
     private JPanel panelMain;
-public TicTacToeInterface() {
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+
+    public TicTacToeInterface() {
     buttonMSG.addActionListener(new ActionListener() { //this overrides actionPerformed function
         @Override
         public void actionPerformed(ActionEvent e) {
