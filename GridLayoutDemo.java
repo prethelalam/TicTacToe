0cca74bc030ee190286c3b91291c07c05087fdcb
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2023-04-29 17:41:30 -0500

    Updated Notes for Game Board

--- GridLayoutDemo.java
+++ GridLayoutDemo.java
@@ -20,4 +20,19 @@ public class GridLayoutDemo {
         panel.add(button4);
         frame.add(panel);
     }
+
+
+    //        super(); //this calls the parent class, which is the jframe constructor
+//        pane = getContentPane(); //don't have to use this. because that is for the user. so you just have to put getcontentpane since jframe has that
+//        pane.setLayout(new GridLayout(3, 3));
+//        setTitle("TicTacToe"); //this is the title of the game obviously
+//        setSize(500, 500); //this is the size of the panel
+//        setResizable(false); //this does not let the user to re-size the game
+//        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+//        setVisible(true); //this will allow the user to see the GUI
+//        currentPlayer = "x";
+//        board = new JButton[3][3];
+//        hasWinner = false; //so the game just started and there is not any winner yet
+//        initializeBoard();
+//        initializeMenuBar();
 }
