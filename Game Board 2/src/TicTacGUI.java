69459d6dee547a94fe475193ab82ff07b593ec33
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2023-04-29 17:41:12 -0500

    Updated Game Board

--- Game Board 2/src/TicTacGUI.java
+++ Game Board 2/src/TicTacGUI.java
@@ -19,19 +19,36 @@ public class TicTacGUI extends JFrame {
     private JMenuItem newGame;
 
     public TicTacGUI() { //this is going to be our constructor. this is a method that will be used to initialize objects
-        super(); //this calls the parent class, which is the jframe constructor
-        pane = getContentPane(); //don't have to use this. because that is for the user. so you just have to put getcontentpane since jframe has that
-        pane.setLayout(new GridLayout(3, 3));
-        setTitle("TicTacToe"); //this is the title of the game obviously
-        setSize(500, 500); //this is the size of the panel
-        setResizable(false); //this does not let the user to re-size the game
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setVisible(true); //this will allow the user to see the GUI
-        currentPlayer = "x";
-        board = new JButton[3][3];
+        JFrame frame = new JFrame("Tic Tac Toe");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3,3));
+        JButton button1 = new JButton("");
+        JButton button2 = new JButton("");
+        JButton button3 = new JButton("");
+        JButton button4 = new JButton("");
+        JButton button5 = new JButton("");
+        JButton button6 = new JButton("");
+        JButton button7 = new JButton("");
+        JButton button8 = new JButton("");
+        JButton button9 = new JButton("");
+        panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+        panel.add(button5);
+        panel.add(button6);
+        panel.add(button7);
+        panel.add(button8);
+        panel.add(button9);
+        frame.add(panel);
         hasWinner = false; //so the game just started and there is not any winner yet
         initializeBoard();
         initializeMenuBar();
+
     }
         private void initializeMenuBar () { //this will obviously initialize the menu bar
             menuBar = new JMenuBar();
