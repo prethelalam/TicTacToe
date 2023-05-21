3861f5bb499a4251959da73a521665e41cef76c4
Prethel Alam <108842594+prethelalam@users.noreply.github.com>
2023-05-21 18:46:05 -0500

    Completed Tic Tac Toe GUI

--- TicTacToeLearning.java
+++ TicTacToeLearning.java
@@ -23,6 +23,16 @@ public class TicTacToeLearning {
     private int xCount1=0;
     private int oCount1=0;
     private String startGame = "X";
+    private int b1 = 10;
+    private int b2 = 10;
+    private int b3 = 10;
+    private int b4 = 10;
+    private int b5 = 10;
+    private int b6 = 10;
+    private int b7 = 10;
+    private int b8 = 10;
+    private int b9 = 10;
+    private int i = 0;
 
     /**
      * Launch the application.
@@ -45,6 +55,95 @@ public class TicTacToeLearning {
      */
     public TicTacToeLearning() {
         initialize();
+    }
+
+    private void winningGame(){
+        //for player x to win
+        if (b1 == 1 && b2 == 1 && b3 == 1){
+            JOptionPane.showMessageDialog(frame, "Player X Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            xCount1++;
+            xCount.setText(String.valueOf(xCount1));
+        }
+        else if (b4 == 1 && b5 == 1 && b6 == 1){
+            JOptionPane.showMessageDialog(frame, "Player X Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            xCount1++;
+            xCount.setText(String.valueOf(xCount1));
+        }
+        else if (b7 == 1 && b8 == 1 && b9 == 1){
+            JOptionPane.showMessageDialog(frame, "Player X Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            xCount1++;
+            xCount.setText(String.valueOf(xCount1));
+        }
+        else if (b1 == 1 && b4 == 1 && b7 == 1){
+            JOptionPane.showMessageDialog(frame, "Player X Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            xCount1++;
+            xCount.setText(String.valueOf(xCount1));
+        }
+        else if (b2 == 1 && b5 == 1 && b8 == 1){
+            JOptionPane.showMessageDialog(frame, "Player X Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            xCount1++;
+            xCount.setText(String.valueOf(xCount1));
+        }
+        else if (b3 == 1 && b6 == 1 && b9 == 1){
+            JOptionPane.showMessageDialog(frame, "Player X Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            xCount1++;
+            xCount.setText(String.valueOf(xCount1));
+        }
+        else if (b1 == 1 && b5 == 1 && b9 == 1){
+            JOptionPane.showMessageDialog(frame, "Player X Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            xCount1++;
+            xCount.setText(String.valueOf(xCount1));
+        }
+        else if (b3 == 1 && b5 == 1 && b7 == 1){
+            JOptionPane.showMessageDialog(frame, "Player X Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            xCount1++;
+            xCount.setText(String.valueOf(xCount1));
+        }
+
+        //For player O to win
+        if (b1 == 0 && b2 == 0 && b3 == 0){
+            JOptionPane.showMessageDialog(frame, "Player O Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            oCount1++;
+            oCount.setText(String.valueOf(oCount1));
+        }
+        else if (b4 == 0 && b5 == 0 && b6 == 0){
+            JOptionPane.showMessageDialog(frame, "Player O Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            oCount1++;
+            oCount.setText(String.valueOf(oCount1));
+        }
+        else if (b7 == 0 && b8 == 0 && b9 == 0){
+            JOptionPane.showMessageDialog(frame, "Player O Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            oCount1++;
+            oCount.setText(String.valueOf(oCount1));
+        }
+        else if (b1 == 0 && b4 == 0 && b7 == 0){
+            JOptionPane.showMessageDialog(frame, "Player O Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            oCount1++;
+            oCount.setText(String.valueOf(oCount1));
+        }
+        else if (b2 == 0 && b5 == 0 && b8 == 0){
+            JOptionPane.showMessageDialog(frame, "Player O Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            oCount1++;
+            oCount.setText(String.valueOf(oCount1));
+        }
+        else if (b3 == 0 && b6 == 0 && b9 == 0){
+            JOptionPane.showMessageDialog(frame, "Player O Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            oCount1++;
+            oCount.setText(String.valueOf(oCount1));
+        }
+        else if (b1 == 0 && b5 == 0 && b9 == 0){
+            JOptionPane.showMessageDialog(frame, "Player O Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            oCount1++;
+            oCount.setText(String.valueOf(oCount1));
+        }
+        else if (b3 == 0 && b5 == 0 && b7 == 0){
+            JOptionPane.showMessageDialog(frame, "Player O Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+            oCount1++;
+            oCount.setText(String.valueOf(oCount1));
+        }
+        else if (i == 9){
+            JOptionPane.showMessageDialog(frame, "No One Wins", "Tic Tac Toe", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 
     private void choosePlayer(){
@@ -83,11 +182,16 @@ public class TicTacToeLearning {
                 btn1.setText(startGame);
                 if (startGame.equalsIgnoreCase("X")){
                     btn1.setForeground(Color.RED);
+                    b1 = 1;
+                    i++;
                 }
                 else {
                     btn1.setForeground(Color.blue);
+                    b1 = 0;
+                    i++;
                 }
                 choosePlayer();
+                winningGame();
             }
         });
         btn1.setFont(new Font("Lucida Grande", Font.BOLD, 40));
@@ -99,9 +203,25 @@ public class TicTacToeLearning {
         panel_2.setLayout(new BorderLayout(0, 0));
 
         JButton btn2 = new JButton("");
+        btn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                btn2.setText(startGame);
+                if (startGame.equalsIgnoreCase("X")){
+                    btn2.setForeground(Color.RED);
+                    b2 = 1;
+                    i++;
+                }
+                else {
+                    btn2.setForeground(Color.blue);
+                    b2 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
+            }
+        });
         btn2.setFont(new Font("Lucida Grande", Font.BOLD, 40));
-        btn2.setForeground(Color.BLACK);
-        btn2.setBackground(Color.BLACK);
         panel_2.add(btn2, BorderLayout.CENTER);
 
         JPanel panel_3 = new JPanel();
@@ -110,9 +230,25 @@ public class TicTacToeLearning {
         panel_3.setLayout(new BorderLayout(0, 0));
 
         JButton btn3 = new JButton("");
+        btn3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                btn3.setText(startGame);
+                if (startGame.equalsIgnoreCase("X")){
+                    btn3.setForeground(Color.RED);
+                    b3 = 1;
+                    i++;
+                }
+                else {
+                    btn3.setForeground(Color.blue);
+                    b3 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
+            }
+        });
         btn3.setFont(new Font("Lucida Grande", Font.BOLD, 40));
-        btn3.setForeground(Color.BLACK);
-        btn3.setBackground(Color.BLACK);
         panel_3.add(btn3, BorderLayout.CENTER);
 
         JPanel panel_4 = new JPanel();
@@ -145,9 +281,25 @@ public class TicTacToeLearning {
         panel_6.setLayout(new BorderLayout(0, 0));
 
         JButton btn4 = new JButton("");
+        btn4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                btn4.setText(startGame);
+                if (startGame.equalsIgnoreCase("X")){
+                    btn4.setForeground(Color.RED);
+                    b4 = 1;
+                    i++;
+                }
+                else {
+                    btn4.setForeground(Color.blue);
+                    b4 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
+            }
+        });
         btn4.setFont(new Font("Lucida Grande", Font.BOLD, 40));
-        btn4.setForeground(Color.BLACK);
-        btn4.setBackground(Color.BLACK);
         panel_6.add(btn4, BorderLayout.CENTER);
 
         JPanel panel_7 = new JPanel();
@@ -156,9 +308,25 @@ public class TicTacToeLearning {
         panel_7.setLayout(new BorderLayout(0, 0));
 
         JButton btn5 = new JButton("");
+        btn5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                btn5.setText(startGame);
+                if (startGame.equalsIgnoreCase("X")){
+                    btn5.setForeground(Color.RED);
+                    b5 = 1;
+                    i++;
+                }
+                else {
+                    btn5.setForeground(Color.blue);
+                    b5 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
+            }
+        });
         btn5.setFont(new Font("Lucida Grande", Font.BOLD, 40));
-        btn5.setForeground(Color.BLACK);
-        btn5.setBackground(Color.BLACK);
         panel_7.add(btn5, BorderLayout.CENTER);
 
         JPanel panel_8 = new JPanel();
@@ -167,9 +335,25 @@ public class TicTacToeLearning {
         panel_8.setLayout(new BorderLayout(0, 0));
 
         JButton btn6 = new JButton("");
+        btn6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                btn6.setText(startGame);
+                if (startGame.equalsIgnoreCase("X")){
+                    btn6.setForeground(Color.RED);
+                    b6 = 1;
+                    i++;
+                }
+                else {
+                    btn6.setForeground(Color.blue);
+                    b6 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
+            }
+        });
         btn6.setFont(new Font("Lucida Grande", Font.BOLD, 40));
-        btn6.setForeground(Color.BLACK);
-        btn6.setBackground(Color.BLACK);
         panel_8.add(btn6, BorderLayout.CENTER);
 
         JPanel panel_9 = new JPanel();
@@ -202,9 +386,25 @@ public class TicTacToeLearning {
         panel_11.setLayout(new BorderLayout(0, 0));
 
         JButton btn7 = new JButton("");
+        btn7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                btn7.setText(startGame);
+                if (startGame.equalsIgnoreCase("X")){
+                    btn7.setForeground(Color.RED);
+                    b7 = 1;
+                    i++;
+                }
+                else {
+                    btn7.setForeground(Color.blue);
+                    b7 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
+            }
+        });
         btn7.setFont(new Font("Lucida Grande", Font.BOLD, 40));
-        btn7.setForeground(Color.BLACK);
-        btn7.setBackground(Color.BLACK);
         panel_11.add(btn7, BorderLayout.CENTER);
 
         JPanel panel_12 = new JPanel();
@@ -213,9 +413,25 @@ public class TicTacToeLearning {
         panel_12.setLayout(new BorderLayout(0, 0));
 
         JButton btn8 = new JButton("");
+        btn8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                btn8.setText(startGame);
+                if (startGame.equalsIgnoreCase("X")){
+                    btn8.setForeground(Color.RED);
+                    b8 = 1;
+                    i++;
+                }
+                else {
+                    btn8.setForeground(Color.blue);
+                    b8 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
+            }
+        });
         btn8.setFont(new Font("Lucida Grande", Font.BOLD, 40));
-        btn8.setForeground(Color.BLACK);
-        btn8.setBackground(Color.BLACK);
         panel_12.add(btn8, BorderLayout.CENTER);
 
         JPanel panel_13 = new JPanel();
@@ -224,9 +440,25 @@ public class TicTacToeLearning {
         panel_13.setLayout(new BorderLayout(0, 0));
 
         JButton btn9 = new JButton("");
+        btn9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                btn9.setText(startGame);
+                if (startGame.equalsIgnoreCase("X")){
+                    btn9.setForeground(Color.RED);
+                    b9 = 1;
+                    i++;
+                }
+                else {
+                    btn9.setForeground(Color.blue);
+                    b9 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
+            }
+        });
         btn9.setFont(new Font("Lucida Grande", Font.BOLD, 40));
-        btn9.setForeground(Color.BLACK);
-        btn9.setBackground(Color.BLACK);
         panel_13.add(btn9, BorderLayout.CENTER);
 
         JPanel panel_14 = new JPanel();
@@ -246,6 +478,16 @@ public class TicTacToeLearning {
                 btn7.setText(null);
                 btn8.setText(null);
                 btn9.setText(null);
+                b1 = 10;
+                b2 = 10;
+                b3 = 10;
+                b4 = 10;
+                b5 = 10;
+                b6 = 10;
+                b7 = 10;
+                b8 = 10;
+                b9 = 10;
+                i = 0;
             }
         });
         btnReset.setBackground(Color.BLACK);
